9606e3c11923dbfae11227e97f76e34fb8206893
andrey <polyakovv.andrey.01@gmail.com>
2025-05-16 12:53:07 +0300

    add manage subscription button

--- api/payment/urls.py
+++ api/payment/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PaymentMethodListView
+from .views import PaymentMethodListView, CreateBillingPortalSession
 from .webhooks import stripe_webhook
 
 urlpatterns = [
     path('payment-methods/', PaymentMethodListView.as_view(), name='payment-methods'),
+    path('billing-portal/', CreateBillingPortalSession.as_view(), name='billing-portal'),
     path('webhook/', stripe_webhook, name='stripe-webhook'),
-] 
+]
--- api/payment/views.py
+++ api/payment/views.py
@@ -1,10 +1,14 @@
 import stripe
 from django.conf import settings
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+
+from saas_template_backend.settings import SUBSCRIPTION_RETURN_URL
 from .models import PaymentMethod
 from .serializers import PaymentMethodSerializer
+from ..subscription.models import Subscription
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -16,3 +20,26 @@ class PaymentMethodListView(APIView):
         payment_methods = PaymentMethod.objects.filter(user=request.user)
         serializer = PaymentMethodSerializer(payment_methods, many=True)
         return Response(serializer.data)
+
+
+class CreateBillingPortalSession(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            sub = Subscription.objects.filter(user=request.user, status="active").first()
+            if not sub:
+                return Response({'detail': 'No subscription found'}, status=status.HTTP_404_NOT_FOUND)
+            customer_id = sub.stripe_customer_id
+        except Subscription.DoesNotExist:
+            return Response({'detail': 'No subscription found'}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            session = stripe.billing_portal.Session.create(
+                customer=customer_id,
+                return_url=SUBSCRIPTION_RETURN_URL
+            )
+        except stripe.error.StripeError as e:
+            return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'url': session.url}, status=status.HTTP_200_OK)
